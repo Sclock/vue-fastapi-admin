510d0333b884318329bc7fb7405a85a8b7847c05
sclock <1342810270@qq.com>
2024-05-28 04:23:20 +0800

    修改数据库和静态文件使用方式从.env读取，未配置.env的默认使用sqlite和不提供静态网页服务

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,20 @@
-from fastapi import FastAPI
+import logging
+import os
+from http.client import HTTPException
+
+from dotenv import load_dotenv
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from starlette.exceptions import HTTPException
+from starlette.status import HTTP_404_NOT_FOUND
 
 from app.core.exceptions import SettingNotFound
-from app.core.init_app import (
-    init_menus,
-    init_superuser,
-    make_middlewares,
-    register_db,
-    register_exceptions,
-    register_routers,
-)
+from app.core.init_app import (init_menus, init_superuser, make_middlewares,
+                               register_db, register_exceptions,
+                               register_routers)
+
+logger = logging.getLogger("uvicorn")
 
 try:
     from app.settings.config import settings
@@ -16,7 +22,86 @@ except ImportError:
     raise SettingNotFound("Can not import settings")
 
 
+def load_static(app: FastAPI):
+    """
+    是否由FastAPI提供静态文件服务
+    """
+    app.mount("/", StaticFiles(directory="web/dist", html=True), name="dist")
+
+    @app.exception_handler(HTTPException)
+    async def _(request: Request, exc: HTTPException):
+        # 检查异常的状态码是否为 404
+        if exc.status_code == HTTP_404_NOT_FOUND:
+            # 获取请求路径
+            path = request.url.path
+            # 检查路径是否不是以 /api 开头
+            if not path.startswith("/api"):
+                # 如果不是以 /api 开头，则重定向到根路径
+                return RedirectResponse(url="/")
+            # 如果是以 /api 开头，返回适当的 JSON 错误信息
+            return JSONResponse(status_code=HTTP_404_NOT_FOUND, content={"detail": "Not Found"})
+        # 对于其他的HTTP异常，返回JSON格式的错误详情
+        return JSONResponse(status_code=exc.status_code, content={"detail": exc.detail})
+
+
+def load_dotenv_file(app: FastAPI):
+    """
+    从环境变量中加载配置文件
+    """
+    # 是否由FastAPI提供静态文件服务
+    if os.getenv("LOAD_STATIC_FROM_LOCAL") == "true":
+        logger.info("使用FastAPI提供静态网页服务")
+        load_static(app)
+    else:
+        logger.warning("未使用FastAPI提供静态网页服务,请单独启动静态网页服务!")
+
+    # 使用的数据库类型
+    db_type = os.getenv("DATABASE_TYPE")
+    settings.TORTOISE_ORM = build_orm_config(db_type)
+
+
+def build_orm_config(db_type: str):
+    """
+    构建ORM配置
+    """
+    if db_type == "mysql":
+        logger.info("当前使用MySQL数据库")
+        credentials = {
+            "host": os.getenv("MYXQL_HOST"),
+            "port": os.getenv("MYXQL_PORT"),
+            "user": os.getenv("MYXQL_USER"),
+            "password": os.getenv("MYXQL_PASSWORD"),
+            "database": os.getenv("MYXQL_DATABASE"),
+        }
+    elif db_type == "sqlite":
+        logger.info("当前使用SQLite数据库")
+        credentials = {"file_path": f"{settings.BASE_DIR}/db.sqlite3"}
+    else:
+        logger.info("未提供数据库类型,默认使用使用SQLite数据库")
+        credentials = {"file_path": f"{settings.BASE_DIR}/db.sqlite3"}
+
+    return {
+        "connections": {
+            db_type: {
+                "engine": f"tortoise.backends.{db_type}",
+                "credentials": credentials,
+            }
+        },
+        "apps": {
+            "models": {
+                "models": ["app.models"],
+                "default_connection": db_type,
+            },
+        },
+        "use_tz": False,
+        "timezone": "Asia/Shanghai",
+    }
+
+
 def create_app() -> FastAPI:
+    # 从环境变量中加载配置文件
+    load_dotenv()
+
     app = FastAPI(
         title=settings.APP_TITLE,
         description=settings.APP_DESCRIPTION,
@@ -24,6 +109,9 @@ def create_app() -> FastAPI:
         openapi_url="/openapi.json",
         middleware=make_middlewares(),
     )
+
+    load_dotenv_file(app)
+
     register_db(app)
     register_exceptions(app)
     register_routers(app, prefix="/api")
--- app/settings/config.py
+++ app/settings/config.py
@@ -16,43 +16,17 @@ class Settings(BaseSettings):
     CORS_ALLOW_HEADERS: typing.List = ["*"]
 
     DEBUG: bool = True
-    DB_URL: str = "sqlite://db.sqlite3"
-    DB_CONNECTIONS: dict = {
-        "default": {
-            "engine": "tortoise.backends.sqlite",
-            "db_url": DB_URL,
-            "credentials": {
-                "host": "",
-                "port": "",
-                "user": "",
-                "password": "",
-                "database": "",
-            },
-        },
-    }
 
-    PROJECT_ROOT: str = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+    PROJECT_ROOT: str = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), os.pardir))
     BASE_DIR: str = os.path.abspath(os.path.join(PROJECT_ROOT, os.pardir))
     LOGS_ROOT: str = os.path.join(BASE_DIR, "app/logs")
-    SECRET_KEY: str = "3488a63e1765035d386f05409663f55c83bfae3b3c61a932744b20ad14244dcf"  # openssl rand -hex 32
+    # openssl rand -hex 32
+    SECRET_KEY: str = "3488a63e1765035d386f05409663f55c83bfae3b3c61a932744b20ad14244dcf"
     JWT_ALGORITHM: str = "HS256"
     JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7  # 7 day
-    TORTOISE_ORM: dict = {
-        "connections": {
-            "sqlite": {
-                "engine": "tortoise.backends.sqlite",
-                "credentials": {"file_path": f"{BASE_DIR}/db.sqlite3"},
-            }
-        },
-        "apps": {
-            "models": {
-                "models": ["app.models"],
-                "default_connection": "sqlite",
-            },
-        },
-        "use_tz": False,
-        "timezone": "Asia/Shanghai",
-    }
+
+    TORTOISE_ORM: dict = {}
     DATETIME_FORMAT: str = "%Y-%m-%d %H:%M:%S"
 
 
